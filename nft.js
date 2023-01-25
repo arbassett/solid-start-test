f050795169609ecbfc3269428b008a8f8d0e91b2
Aaron Bassett <arbassett4@outlook.com>
2023-01-25 12:21:03 -0500

    nft relative paths

--- nft.js
+++ nft.js
@@ -1,6 +1,11 @@
 import { nodeFileTrace } from '@vercel/nft';
 import fs from 'node:fs'
+import path from 'node:path'
 const file = './.vercel/output/functions/render.func/index.js';
-const { fileList } = await nodeFileTrace([file]);
+const { fileList } = await nodeFileTrace([file],{
+    // processCwd: './.vercel/output/functions/render.func',
+});
 
-fs.writeFileSync(`${file}.nft.json`, JSON.stringify({version: 1, files: Array.from(fileList)}))
+fs.writeFileSync(`${file}.nft.json`, JSON.stringify({version: 1, files: Array.from(fileList).map((file) =>  path
+    .relative('./.vercel/output/functions/render.func', file)
+    .replace(/\\/g, '/'))}))
